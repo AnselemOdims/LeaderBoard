7abafbbd9c7a23f683d81982ddb221ccabfcf2ec
Anselem <anselemodims@gmail.com>
2021-12-14 09:08:18 +0100

    Feat: created the Helper class

--- src/helpers.js
+++ src/helpers.js
@@ -2,3 +2,10 @@ import Utils from './utils.js';
 import FetchWrapper from './fetchWrapper.js';
 
 const API = new FetchWrapper('https://us-central1-js-capstone-backend.cloudfunctions.net/api/');
+
+/**
+ * @class Helper - the helper class
+ */
+export default class Helper {
+
+}
