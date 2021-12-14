632b95494c5f1abbf573f9bb9f20ea4764a80666
Anselem <anselemodims@gmail.com>
2021-12-14 09:25:10 +0100

    Feat: created an eventlistener for the window object and invoked all necessary functions

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import Helper from './helpers.js';
 import Utils from './utils.js';
 
 const helper = new Helper();
+
+window.addEventListener('DOMContentLoaded', async () => {
+  await helper.createNewGame();
+  await helper.getHandler();
+  Utils.inputFocus();
+  document.querySelector('.add-form form').addEventListener('submit', helper.postHandler);
+  document.querySelector('.refresh-btn').addEventListener('click', helper.getHandler);
+});
