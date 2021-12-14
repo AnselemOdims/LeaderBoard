d8635e42112a779d39e376fd97153dc2d8851918
Anselem <anselemodims@gmail.com>
2021-12-14 07:53:07 +0100

    Feat: created a Utils static class method

--- src/utils.js
+++ src/utils.js
@@ -6,5 +6,13 @@ const API = new FetchWrapper('https://us-central1-js-capstone-backend.cloudfunct
  * @class Utils - the utility class to hold all utiltity functionality
  */
 export default class Utils {
-
+/**
+ * @function startLoader - handles the spinner loading
+ * @param {HTMLElement} elem1 - the first HTMLElement
+ * @param {HTMLElement} elem2 - the second HTMLElement
+ */
+  static startLoader(elem1, elem2) {
+    elem2.classList.add('loading-spinner');
+    elem1.innerHTML = 'Loading';
+  }
 }
