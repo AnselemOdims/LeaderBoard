0f0f7fa4eb8e59a754f2c1068e9ff61afe4da530
Anselem <anselemodims@gmail.com>
2021-12-14 07:50:12 +0100

    Feat: created the Utils class

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,10 @@
 import FetchWrapper from './fetchWrapper.js';
 
 const API = new FetchWrapper('https://us-central1-js-capstone-backend.cloudfunctions.net/api/');
+
+/**
+ * @class Utils - the utility class to hold all utiltity functionality
+ */
+export default class Utils {
+
+}
