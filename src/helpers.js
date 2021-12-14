0f4d15c0ebb2e9346ca6b8c95f092e54c22a9ffe
Anselem <anselemodims@gmail.com>
2021-12-14 09:18:05 +0100

    Feat: Added the postHandler instance class method

--- src/helpers.js
+++ src/helpers.js
@@ -3,6 +3,8 @@ import Utils from './utils.js';
 import FetchWrapper from './fetchWrapper.js';
 
 const API = new FetchWrapper('https://us-central1-js-capstone-backend.cloudfunctions.net/api/');
+const spinner = document.querySelector('.spinner');
+const submit = document.querySelector('.submit');
 
 /**
  * @class Helper - the helper class
@@ -19,6 +21,32 @@ export default class Helper {
       } catch (err) {
         throw new Error(err);
       }
+    }
+  }
+
+  /**
+   * @function postHandler - handles the form submit event
+   * @param {Event Object} e - the event object
+   */
+  async postHandler(e) {
+    const user = document.querySelector('#name').value.trim();
+    const score = Number(document.querySelector('#score').value.trim());
+    try {
+      e.preventDefault();
+      Utils.inputChecker();
+      const data = {
+        user,
+        score,
+      };
+      Utils.startLoader(submit, spinner);
+      const { result } = await API.post(`games/${Utils.getLocal()}/scores`, data);
+      Utils.showModal(result);
+    } catch (err) {
+      throw new Error(err);
+    } finally {
+      Utils.stopLoader(submit, spinner, 'Submit');
+      Utils.clearFields();
+      Utils.inputFocus();
     }
   }
 }
