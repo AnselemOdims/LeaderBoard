7b472dbf45b6bdc0caaf019986f90d55fc593df7
Anselem <anselemodims@gmail.com>
2021-12-14 07:56:01 +0100

    Feat: created the stopLoader static class method

--- src/utils.js
+++ src/utils.js
@@ -7,12 +7,23 @@ const API = new FetchWrapper('https://us-central1-js-capstone-backend.cloudfunct
  */
 export default class Utils {
 /**
- * @function startLoader - handles the spinner loading
+ * @function startLoader - handles the start of the spinner loading
  * @param {HTMLElement} elem1 - the first HTMLElement
  * @param {HTMLElement} elem2 - the second HTMLElement
  */
   static startLoader(elem1, elem2) {
     elem2.classList.add('loading-spinner');
     elem1.innerHTML = 'Loading';
+  }
+
+  /**
+   * @function stopLoader - handles the stop of the spinner loading
+   * @param {HTMLElement} elem1 - the first HTMLElement
+   * @param {HTMLElement} elem2 - the second HTMLElement
+   * @param {string} value - the string value to insert into the DOM
+   */
+  static stopLoader(elem1, elem2, value) {
+    elem2.classList.remove('loading-spinner');
+    elem1.innerHTML = value;
   }
 }
